fe2181c7067d0f93581733064d402609d623f25b
Paul Yang <paul.yang1979@gmail.com>
2025-07-01 20:45:15 +1000

    Update create-vm.tf

--- create-vm.tf
+++ create-vm.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.39"
+    }
+  }
+}
+
+
 provider "aws" {
   region = "eu-central-1"  # Change to your preferred region
 }
