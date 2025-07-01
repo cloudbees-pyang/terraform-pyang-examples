f27c76d596bc0f74ceda46dcfedc8e2628dc77f6
Paul Yang <paul.yang1979@gmail.com>
2025-07-01 20:37:39 +1000

    Update create-vm.tf

--- create-vm.tf
+++ create-vm.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-central-1"  # Change to your preferred region
 }
 
-resource "aws_instance" "my_vm" {
+resource "aws_instance" "new_vm" {
   ami           = "ami-000b1869774b5bb6a"  
   instance_type = "t2.micro"
 
